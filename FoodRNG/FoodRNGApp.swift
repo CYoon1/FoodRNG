e79a8713255201026db0147cce5948b5f6fb969d
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-03-15 14:29:33 -0400

    Initial Commit

---
+++ FoodRNG/FoodRNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRNGApp.swift
+//  FoodRNG
+//
+//  Created by Christopher Yoon on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodRNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
